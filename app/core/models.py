3f0b126fbb2f36e343e2a31066afdf87ce750f3b
Artur <petrikartur@gmail.com>
2023-08-02 16:34:36 +0300

    added custom User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,50 @@
-from django.db import models  # noqa: F401
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Class to represent a manager for a User model."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Creates, saves and return a new superuser"""
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Model to represent a user in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    def __str__(self):
+        """Returns readable representation of the model."""
+
+        return f"User {self.id}: {self.email}"
