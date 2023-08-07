1b91b1697a5f8c55e1bafac85ae17133608b761c
Artur <petrikartur@gmail.com>
2023-08-07 16:03:56 +0300

    added manage user API

--- app/users/urls.py
+++ app/users/urls.py
@@ -8,4 +8,5 @@ app_name = "users"
 urlpatterns = [
     path("create/", views.CreateUserAPIView.as_view(), name="create"),
     path("token/", views.CreateTokenAPIView.as_view(), name="token"),
+    path("me/", views.ManageUserAPIView.as_view(), name="me"),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import authentication, generics, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -16,3 +16,16 @@ class CreateTokenAPIView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES  # to enable it in browsable API
+
+
+class ManageUserAPIView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+
+        return self.request.user
